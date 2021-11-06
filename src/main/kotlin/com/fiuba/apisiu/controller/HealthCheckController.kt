73a4bbef0e21c0177c3487aa017b3d3c9229034a
Franco Cerquetti <fd.cerquetti@gmail.com>
2021-11-06 19:36:05 -0300

    Added health check

--- src/main/kotlin/com/fiuba/apisiu/controller/HealthCheckController.kt
+++ src/main/kotlin/com/fiuba/apisiu/controller/HealthCheckController.kt
@@ -1,11 +1,13 @@
 package com.fiuba.apisiu.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/health")
 class HealthCheckController {
 
-    @GetMapping("/")
+    @GetMapping
     fun healthCheck() = "Ok"
 }
